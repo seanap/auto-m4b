70f4ea1cbada2b1bc1d8bdca6fe4a1ffc699707a
seanap <seanap@gmail.com>
2022-04-03 12:39:50 -0400

    Revised Folder Handling
    Now all multi file books are redirected to /mp3merge. Folders with subfolders are redirected to /fix.  Ffprobe now samples the correct file.

--- auto-m4b-tool.sh
+++ auto-m4b-tool.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# set n to 1
-n=1
+# set m to 1
+m=1
 # variable defenition
 inputfolder="/temp/mp3merge/"
 outputfolder="/temp/untagged/"
 originalfolder="/temp/recentlyadded/"
+fixitfolder="/temp/fix"
 backupfolder="/temp/backup/"
 binfolder="/temp/delete/"
 m4bend=".m4b"
@@ -13,49 +14,51 @@ logend=".log"
 #change to the merge folder, keeps this clear and the script could be kept inside the container
 cd "$inputfolder" || return
 
-# continue until $n  5
-while [ $n -ge 0 ]; do
+# continue until $m  5
+while [ $m -ge 0 ]; do
+
+	#copy files to backup destination
+	echo "Making a backup of the whole $originalfolder "
+	cp -Ru "$originalfolder"* $backupfolder
 
 	#make sure all single file mp3's & m4b's are in their own folder
 	echo "Making sure all books are in their own folder"
-	for file in $originalfolder*.{m4b,mp3}; do
+	for file in "$originalfolder"*.{m4b,mp3}; do
 		if [[ -f "$file" ]]; then
 			mkdir "${file%.*}"
 			mv "$file" "${file%.*}"
 		fi
 	done
 
-	echo "Read the mp3 files for movement."
-	readarray -d '' songfile < <(find "$originalfolder" -type f -iname \*.mp3 -print0)
-	IFS='/' read -r -a parts <<<"${songfile[0]}"
+	#Move folders with multiple audiofiles to inputfolder
+	echo "Moving folders with 2 or more audiofiles to $inputfolder "
+	find "$originalfolder" -maxdepth 2 -mindepth 2 -type f \( -name '*.mp3' -o -name '*.m4b' -o -name '*.m4a' \) -print0 | xargs -0 -L 1 dirname | sort | uniq -c | grep -E -v '^ *1 ' | sed 's/^ *[0-9]* //' | while read i; do mv -v "$i" $inputfolder; done
 
-	echo "Making a backup of the whole input/original folder."
-	#copy files to backup destination 
-	cp -Ru "$originalfolder"* $backupfolder
+	#Move folders with nested subfolders to fixitfolder for manual fixing
+	echo "Nested subfolders are BAD moving to $fixitfolder"
+	find "$originalfolder" -maxdepth 3 -mindepth 3 -type f \( -name '*.mp3' -o -name '*.m4b' -o -name '*.m4a' \) -exec sh -c '
+		for f do
+			gp="$(basename "$(dirname "$(dirname "$f")")")"
+			printf "%s\n" "$gp"
+		done
+	' sh-find {} + | sort | uniq -d | while read j; do mv -v "$originalfolder$j" $fixitfolder; done
 
-	#Calculating the place bookfolder for mp3s
-	len=${#parts[@]}
-	lenpath=$len-1
+	#Move single file mp3's to inputfolder
+	echo "Moving single file mp3's to $inputfolder "
+	find "$originalfolder" -maxdepth 2 -type f \( -name '*.mp3' \) -printf "%h\0" | xargs -0 mv -t "$inputfolder"
 
-	#Moving the first book to the merge directory
-	echo "Moving ${parts[lenpath]} to merge it."
-	bookpath=${songfile/"${parts[lenpath]}"}
-	mv "$bookpath" $inputfolder
-
-	#Moving the m4b files to the untagged folder as no Merge needed
-	echo "Moving all the m4b books to untagged."
-	find "$originalfolder" -type f \( -iname \*.m4b -o -iname \*.mp4 -o -iname \*.m4a -o -iname \*.ogg \) -exec mv -f "{}" "$outputfolder" \;
+	#Moving the single m4b files to the untagged folder as no Merge needed
+	echo "Moving all the single m4b books to $outputfolder "
+	find "$originalfolder" -maxdepth 2 -type f \( -iname \*.m4b -o -iname \*.mp4 -o -iname \*.m4a -o -iname \*.ogg \) -printf "%h\0" | xargs -0 mv -t "$outputfolder"
 
 	# clear the folders
 	rm -r "$binfolder"* 2>/dev/null
-	find "$originalfolder"* -type f -size -200k -delete
-	find "$originalfolder"* -type d -exec rmdir {} + 2>/dev/null
 
 	if ls -d */ 2>/dev/null; then
 		echo Folder Detected
 		for book in *; do
 			if [ -d "$book" ]; then
-				mpthree=$(find "$book" -maxdepth 2 -type f -name "*.mp3" | head -n 1)
+				mpthree=$(find "$book" -maxdepth 2 -type f \( -name '*.mp3' -o -name '*.m4b' \) | head -n 1)
 				m4bfile=$outputfolder$book$m4bend
 				logfile=$outputfolder$book$logend
 				echo Sampling $mpthree
@@ -63,7 +66,7 @@ while [ $n -ge 0 ]; do
 				echo Bitrate = $bit
 				echo The folder "$book" will be merged to "$m4bfile"
 				echo Starting Conversion
-				m4b-tool merge "$book" -n -q --audio-bitrate="$bit" --skip-cover --use-filenames-as-chapters --audio-codec=libfdk_aac --jobs=4 --output-file="$m4bfile" --logfile="$logfile"
+				m4b-tool merge "$book" -n -q --audio-bitrate="$bit" --skip-cover --use-filenames-as-chapters --audio-codec=libfdk_aac --jobs=6 --output-file="$m4bfile" --logfile="$logfile"
 				mv "$inputfolder""$book" "$binfolder"
 				mv "$outputfolder""$book".chapters.txt "$outputfolder"chapters
 				echo Finished Converting
