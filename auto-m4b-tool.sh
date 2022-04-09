89ff12a9aa6b1283f6f8e35043ae84d88682424b
seanap <seanap@gmail.com>
2022-04-09 10:42:16 -0400

    New Feature: Manually Fix Chapters
    To fix chapters first run the mp3 book through like normal, then in /untagged open the book folder and edit the *.chapters.txt file and add/remove/rename the chapters,  then move the book folder from /untagged to /merge.  The script will now rechapter the m4b and move everything back to /untagged when done.

--- auto-m4b-tool.sh
+++ auto-m4b-tool.sh
@@ -2,9 +2,10 @@
 # set m to 1
 m=1
 # variable defenition
-originalfolder="/temp/recentlyadded/"
+CPUcores=6
+inputfolder="/temp/mp3merge/"
 outputfolder="/temp/untagged/"
-inputfolder="/temp/merge/"
+originalfolder="/temp/recentlyadded/"
 fixitfolder="/temp/fix"
 backupfolder="/temp/backup/"
 binfolder="/temp/delete/"
@@ -59,16 +60,22 @@ while [ $m -ge 0 ]; do
 		for book in *; do
 			if [ -d "$book" ]; then
 				mpthree=$(find "$book" -maxdepth 2 -type f \( -name '*.mp3' -o -name '*.m4b' \) | head -n 1)
-				m4bfile=$outputfolder$book$m4bend
-				logfile=$outputfolder$book$logend
-				echo Sampling $mpthree
-				bit=$(ffprobe -hide_banner -loglevel 0 -of flat -i "$mpthree" -select_streams a -show_entries format=bit_rate -of default=noprint_wrappers=1:nokey=1)
-				echo Bitrate = $bit
-				echo The folder "$book" will be merged to "$m4bfile"
-				echo Starting Conversion
-				m4b-tool merge "$book" -n -q --audio-bitrate="$bit" --skip-cover --use-filenames-as-chapters --audio-codec=libfdk_aac --jobs=4 --output-file="$m4bfile" --logfile="$logfile"
-				mv "$inputfolder""$book" "$binfolder"
-				mv "$outputfolder""$book".chapters.txt "$outputfolder"chapters
+				m4bfile=$outputfolder$book/$book$m4bend
+				logfile=$outputfolder$book/$book$logend
+				chapters=$(ls "$inputfolder$book"/*chapters.txt 2> /dev/null | wc -l)
+				if [ "$chapters" != "0" ]; then
+					echo Adjusting Chapters
+					mp4chaps -i "$inputfolder""$book"/*$m4bend
+					mv "$inputfolder$book" "$outputfolder"
+				else
+					echo Sampling $mpthree
+					bit=$(ffprobe -hide_banner -loglevel 0 -of flat -i "$mpthree" -select_streams a -show_entries format=bit_rate -of default=noprint_wrappers=1:nokey=1)
+					echo Bitrate = $bit
+					echo The folder "$book" will be merged to "$m4bfile"
+					echo Starting Conversion
+					m4b-tool merge "$book" -n -q --audio-bitrate="$bit" --skip-cover --use-filenames-as-chapters --no-chapter-reindexing --audio-codec=libfdk_aac --jobs="$CPUcores" --output-file="$m4bfile" --logfile="$logfile"
+					mv "$inputfolder$book" "$binfolder"
+				fi
 				echo Finished Converting
 				#make sure all single file m4b's are in their own folder
 				echo Putting the m4b into a folder
@@ -83,6 +90,6 @@ while [ $m -ge 0 ]; do
 		done
 	else
 		echo No folders detected, next run 5min...
-		sleep 5m
+		sleep 1m
 	fi
 done
